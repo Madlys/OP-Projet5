7f01e9fa0b005d6568d58651a40cd1d053b8705f
Clarisse <78051319+Madlys@users.noreply.github.com>
2023-02-22 16:46:18 +0100

    script.js : different implementation with .then() and .catch() for prevent error

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,9 @@
 //stock API URL in var
 const apiUrl = "http://localhost:3000/api/products";
 
+//Product infos for cart
+let productInfos = []
+
 // Defining async function
 async function productPage(url) {
     // Storing response thanks to fetch() method
@@ -9,7 +12,65 @@ async function productPage(url) {
     // Storing API data in form of JSON
     var data = await response.json();
 
-    console.log(window.location.search);
+    // id recovery
+    let id = window.location.search.match(/[^=]*$/gm)[0];
+
+    // product infos recovery
+    let product;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]._id === id) {
+            product = data[i];
+        } else if (data[i].Type) {
+            product = searchDepth(data[i].Type, id);
+        }
+    }
+    console.log(product)
+
+    //Add product image
+    let productImage = document.querySelector(".item__img");
+    let imageElement = document.createElement("img");
+    imageElement.setAttribute("src", product.imageUrl);
+    imageElement.setAttribute("alt", product.altTxt);
+    productImage.appendChild(imageElement);
+
+    //Add product title
+    document.querySelector("#title").innerText = product.name;
+
+    //Add product price
+    document.querySelector("#price").innerText = product.price;
+
+    //Add product description
+    document.querySelector("#description").innerText = product.description;
+
+    //Add product color choices
+    let productColor = document.querySelector("#colors");
+    for (let i = 0; i < product.colors.length; i++) {
+        let color = document.createElement("option");
+        color.setAttribute("value", product.colors[i]);
+        color.innerText = product.colors[i];
+        productColor.appendChild(color);
+    }
+
+    //Memorise color choice
+    /*productColor.setAttribute("onchange", "colorChoice()");
+    function colorChoice() {
+        let colorChosed = productColor.value;
+        console.log(colorChosed)
+    }*/
+
+    //Number of items
+    let counter = document.querySelector('#quantity');
+    let quantity = 0;
+    counter.addEventListener('input', function () {
+        quantity = counter.valueAsNumber;
+    });
+
+    //Add product info to an array
+    function infos() {
+        let infos = [id, quantity, colorChosed]
+        console.log(infos)
+    }
+    document.querySelector("#addToCart").setAttribute("onclick", "infos()");
 
 }
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,47 +1,51 @@
 //stock API URL in var
 const apiUrl = "http://localhost:3000/api/products";
 
-// Defining async function
-async function home(url) {
-    // Storing response thanks to fetch() method
-    const response = await fetch(url);
+// Storing response thanks to fetch() method
+fetch(apiUrl)
+    .then(response => {
+        if (response.ok) {
+            // Storing API data in form of JSON
+            return response.json();
+        }
 
-    // Storing API data in form of JSON
-    var data = await response.json();
+        //if error, message report by line 50
+        throw new Error('Y a un problème chef !');
+    })
+    .then(data => {
+        //Iterate on data
+        for (i = 0; i < data.length; i++) {
+            //Create HTML anchor
+            let itemElement = document.querySelector("#items");
+            let a = document.createElement("a");
+            let productFrontUrl = new URL("product.html", 'http://127.0.0.1:5500/front/html/?id=ergreg&categorie=rgbdbre&');
+            productFrontUrl.searchParams.append('id', data[i]._id);
+            a.setAttribute("href", productFrontUrl);
+            itemElement.appendChild(a);
 
-    //Generate similar HTML for each product, for a dynamic display
-    for (i = 0; i < data.length; i++) {
-        //Create HTML anchor
-        let itemElement = document.querySelector("#items");
-        let a = document.createElement("a");
-        let params = new URLSearchParams("id=" + data[i]._id);
-        let Url = "./product.html?" + params;
-        a.setAttribute("href", Url);
-        // or shorter => a.setAttribute("href", "./product.html?" + URLSearchParams("id", data[i]._id).toString());
-        // or shorter, using object data => a.setAttribute("href", "./product.html?id=" + data[i]._id);
-        itemElement.appendChild(a);
+            let article = document.createElement("article");
+            a.appendChild(article);
 
-        let article = document.createElement("article");
-        a.appendChild(article);
+            //Add product image
+            let imageElement = document.createElement("img");
+            imageElement.setAttribute("src", data[i].imageUrl);
+            imageElement.setAttribute("alt", data[i].altTxt);
+            article.appendChild(imageElement);
 
-        //Add product image
-        let imageElement = document.createElement("img");
-        imageElement.setAttribute("src", data[i].imageUrl);
-        imageElement.setAttribute("alt", data[i].altTxt);
-        article.appendChild(imageElement);
+            //Add product title/name
+            let h3 = document.createElement("h3");
+            h3.setAttribute("class", "productName");
+            h3.innerText = data[i].name;
+            article.appendChild(h3);
 
-        //Add product title/name
-        let h3 = document.createElement("h3");
-        h3.setAttribute("class", "productName");
-        h3.innerText = data[i].name;
-        article.appendChild(h3);
-
-        //Add product description
-        let p = document.createElement("p");
-        p.setAttribute("class", "productDescription");
-        p.innerText = data[i].description;
-        article.appendChild(p);
-    }
-}
-// Calling that async function
-home(apiUrl);
+            //Add product description
+            let p = document.createElement("p");
+            p.setAttribute("class", "productDescription");
+            p.innerText = data[i].description;
+            article.appendChild(p);
+        }
+    })
+    .catch(error => {
+        //alert messsage if error
+        alert(error.message);
+    });
